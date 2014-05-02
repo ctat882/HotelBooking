44788ae3d90aa0873255b8e1063ed0c2ad3160a7
Giridhaar <Giridhaar@users.noreply.github.com>
2014-05-03 00:43:04 +1000

    Update HotelOwnerDao.java

--- src/edu/unsw/comp9321/HotelOwnerDao.java
+++ src/edu/unsw/comp9321/HotelOwnerDao.java
@@ -1,9 +1,15 @@
 package edu.unsw.comp9321.jdbc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface HotelOwnerDAO {
 
 	public List<HotelOccupancyDTO> getRooms();
-		
+	// get the total number of hotels in the database
+	public int getTotalHotels();
+	// get a list of all the hotel locations
+	public ArrayList<String> getHotelLocList();
+	// get the location of one hotel based on its hotelID
+	public String getHotelLoc(int hotelID);
 }
