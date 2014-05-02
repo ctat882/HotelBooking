5b3974091322a608ff47843354dd85a17ca769c9
Giridhaar <Giridhaar@users.noreply.github.com>
2014-05-02 20:22:27 +1000

    added price check for Executive ($250.00)

--- sql_files/hotel-create.sql
+++ sql_files/hotel-create.sql
@@ -16,11 +16,11 @@ CREATE TABLE Rooms (
 	hotel			INTEGER,
 	size			VARCHAR(20) NOT NULL,
 	price			DECIMAL(5,2) NOT NULL,
-	availability	VARCHAR(20) NOT NULL DEFAULT 'Available',
+	availability	VARCHAR(20) DEFAULT 'Available',
 	-- Constraints
 	CONSTRAINT availability_ck CHECK (availability IN ('Occupied', 'Maintenance', 'Available')),
 	CONSTRAINT size_ck CHECK (size IN ('Single', 'Twin', 'Queen', 'Executive', 'Suite')),
-	CONSTRAINT price_ck CHECK (price IN (70.00,120.00,180.00,300.00)),
+	CONSTRAINT price_ck CHECK (price IN (70.00,120.00,180.00,250.00,300.00)),
 	-- Keys
 	FOREIGN KEY (hotel) REFERENCES Hotels(id),
 	PRIMARY KEY(room_num,hotel)
@@ -51,24 +51,22 @@ CREATE TABLE Bookings (
 );
 
 -- Discount Table
-CREATE TABLE Discounts (
-	discount_id		INTEGER GENERATED ALWAYS AS IDENTITY,
-	hotel			INTEGER,
-	room_type		VARCHAR(20) NOT NULL,
-	start_date		DATE NOT NULL,
-	end_date		DATE NOT NULL,
-	discount		INTEGER NOT NULL,
-	-- Constraints
-	-- Ensure that the discount is not zero and is not greater than 100
-	CONSTRAINT discounts_disc_ck CHECK (discount <= 100 AND discount > 0),
-	CONSTRAINT discounts_room_type_ck CHECK (room_type IN ('Single', 'Twin', 'Queen', 'Executive', 'Suite')),
-	-- Ensure that start date is before or equal to the end date
-	CONSTRAINT discounts_start_ck CHECK (start_date <= end_date),
-	-- Ensure that the end date is after or equal to the start date
-	CONSTRAINT discount_end_ck CHECK (end_date >= start_date),
-	-- Keys
-	FOREIGN KEY(hotel) REFERENCES Hotels(id),
-	PRIMARY KEY(discount_id)
-	
-);
-
+ CREATE TABLE Discounts (
+ 	discount_id		INTEGER GENERATED ALWAYS AS IDENTITY,
+ 	hotel			INTEGER,
+ 	room_type		VARCHAR(20) NOT NULL,
+ 	start_date		DATE NOT NULL,
+ 	end_date		DATE NOT NULL,
+ 	discount		INTEGER NOT NULL,
+ 	-- Constraints
+ 	-- Ensure that the discount is not zero and is not greater than 100
+ 	CONSTRAINT discounts_disc_ck CHECK (discount <= 100 AND discount > 0),
+ 	CONSTRAINT discounts_room_type_ck CHECK (room_type IN ('Single', 'Twin', 'Queen', 'Executive', 'Suite')),
+ 	-- Ensure that start date is before or equal to the end date
+ 	CONSTRAINT discounts_start_ck CHECK (start_date <= end_date),
+ 	-- Ensure that the end date is after or equal to the start date
+ 	CONSTRAINT discount_end_ck CHECK (end_date >= start_date),
+ 	-- Keys
+ 	FOREIGN KEY(hotel) REFERENCES Hotels(id),
+ 	PRIMARY KEY(discount_id) 	
+ );
