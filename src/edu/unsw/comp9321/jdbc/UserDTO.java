6174a5ee3647dc1f703884438b207916d0049119
Giridhaar <giridhaar.g@gmail.com>
2014-05-08 13:58:08 +1000

    added hoteLoc String

--- src/edu/unsw/comp9321/jdbc/UserDTO.java
+++ src/edu/unsw/comp9321/jdbc/UserDTO.java
@@ -5,12 +5,18 @@ public class UserDTO {
 	String username;
 	String password;
 	String hoteLoc;
+	int userHotelID;
 	
 	public UserDTO(String username, String password, String hotelLoc){
 		this.username = username;
 		this.password = password;
-		this.hoteLoc = hotelLoc;
+		this.hoteLoc = hotelLoc;		
 	}
+	
+	public void setUserHotelId(int userHotelID){
+		this.userHotelID = userHotelID;
+	}
+	
 	
 	public String getUsername() {
 		return username;
@@ -23,6 +29,11 @@ public class UserDTO {
 	public String getHoteLoc() {
 		return hoteLoc;
 	}
+	
+	public int getUserHotelId(){
+		return userHotelID;
+	}
+	
 	
 
 	
